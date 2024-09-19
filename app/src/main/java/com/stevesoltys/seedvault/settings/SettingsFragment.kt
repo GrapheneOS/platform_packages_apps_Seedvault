25db22442a34fe1719c2f913383ce7aec93b9880
Daniel Micay <daniel.micay@grapheneos.org>
2024-09-19 03:58:30 -0400

    remove about action until post-fork rebranding

--- app/src/main/java/com/stevesoltys/seedvault/settings/SettingsFragment.kt
+++ app/src/main/java/com/stevesoltys/seedvault/settings/SettingsFragment.kt
@@ -213,13 +213,6 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 .commit()
             true
         }
-        R.id.action_about -> {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment, AboutDialogFragment())
-                .addToBackStack(AboutDialogFragment.TAG)
-                .commit()
-            true
-        }
         else -> false
     }
 
